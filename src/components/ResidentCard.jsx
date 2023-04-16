4df7bfdfc8dfa381856a2b9ed1707a020ea52d63
cordovabarbara <aliendocordova@gmail.com>
2023-04-16 17:29:24 -0500

    add in principal box css, display flex and wrap

--- src/components/ResidentCard.jsx
+++ src/components/ResidentCard.jsx
@@ -10,19 +10,32 @@ const ResidentCard = ({url}) => {
     <header className="resident__header">
       <img className="resident__avatar" src={residents?.image} alt=""/>
         <div className="resident__status">
-          <div className="resident__circle"></div>
+          <div className={`resident__circle ${residents?.status}`}></div>
             <span className="resident__label">{residents?.status}</span>
           </div>
     </header>
 
-    <h3 className="resident__name">{residents?.name}</h3>
+<div className="resident__box">
+  <h3 className="resident__name">{residents?.name}</h3>
     <hr className="resident__hr" />
-    <ul className="resident__list">
-        <li>Specie: {residents?.species} </li>
-        <li>Origin: {residents?.origin.name}</li>
-        <li>Episodes where appear: {residents?.episode.length}</li>
-    </ul>
+      <ul className="resident__list">
+        <li className="list__item">
+        <span className="resident__label">Specie: </span>
+        <span className="resident__value">{residents?.species}</span>
+        </li>
+
+        <li className="list__item">
+        <span className="resident__label">Origin: </span> 
+        <span className="resident__value">{residents?.origin.name}</span>
+        </li>
+
+        <li className="list__item">
+        <span className="resident__label">Episodes where appear: </span> 
+        <span className="resident__value">{residents?.episode.length}</span>
+        </li>
+      </ul>
     </div>
+  </div>
   )
 }
 
