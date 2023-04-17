68728a06a173bb6c746820148ffb27d7166d65e2
cordovabarbara <aliendocordova@gmail.com>
2023-04-16 20:11:16 -0500

    added className to cardLocation, added styling and colors

--- src/components/CardLocation.jsx
+++ src/components/CardLocation.jsx
@@ -2,17 +2,26 @@ import './Styles/CardLocation.css'
 
 const CardLocation = ({ location }) => {
   return (
-    <div>
-    <h2>{location?.name}</h2>
-    <ul>
-        <li>Type: {location?.type}</li>
-        <li>Dimension: {location?.dimension}</li>
-        <li>Population: {location?.residents.length}</li>
+    <div className='location'>
+    <h2 className='location__name'>{location?.name}</h2>
+    <ul className='location__list'>
+      <li className='location__item'>
+        <span className='location__info'>Type: </span>
+        {location?.type}
+      </li>
+
+    <li className='location__item'>
+      <span className='location__info'> Dimension: </span>
+        {location?.dimension}
+    </li>
+
+    <li className='location__item'>
+        <span  className='location__info'>Population: </span>
+        {location?.residents.length}
+      </li>
     </ul>
-
-
-    </div>
-  )
-}
+  </div>
+  );
+};
 
 export default CardLocation
